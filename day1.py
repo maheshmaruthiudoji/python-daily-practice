f849783d50968778dc2f235323599fce5f33121d
Mahesh M UDOJI <maheshudoji8088@gmail.com>
2026-04-05 23:40:58 +0530

    Day 2: loops practice

--- day1.py
+++ day1.py
@@ -1,1 +1,15 @@
-print("Hello world")
+rows = 4
+
+for i in range(1, rows + 1):
+    print("*" * i)
+
+
+
+
+
+
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows + 1):
+    print("*" * i)   
