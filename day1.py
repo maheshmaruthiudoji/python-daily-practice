8a6d86b27b221cce24b0b18f56f7fd94c9842c5c
Mahesh Maruti Udoji <maheshudoji8088@gmail.com>
2026-04-06 16:20:06 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,15 +1,1 @@
-rows = 4
-
-for i in range(1, rows + 1):
-    print("*" * i)
-
-
-
-
-
-
-
-rows = int(input("Enter number of rows: "))
-
-for i in range(1, rows + 1):
-    print("*" * i)   
+print("Hello world")
