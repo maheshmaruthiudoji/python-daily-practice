6e4b90dbc2bf0f5182ccf6aec7c02960c4b62acd
Mahesh M UDOJI <maheshudoji8088@gmail.com>
2026-04-08 16:03:19 +0530

    Day 3 & 4: practice programs

---
+++ day4.py
@@ -0,0 +1,19 @@
+str="mahesh maruti udoji"
+print(str.count("a"))
+print(str.upper())
+
+# conditonal statements
+age=65
+if(age <= 25):
+    print("condier government exam")
+elif(age <=90):
+    print("not consider")
+
+
+number=-23
+if(number > 0):
+    print("positive")
+elif(number < 0):
+    print("negative")
+
+
