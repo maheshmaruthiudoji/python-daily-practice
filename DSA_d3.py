324b3a763142e047eb41244a52baac49416e8a50
Mahesh M UDOJI <maheshudoji8088@gmail.com>
2026-04-24 23:16:16 +0530

    array

---
+++ DSA_d3.py
@@ -0,0 +1,20 @@
+#To remove duplicates in-place from a sorted array
+def removeDuplicates(nums):
+    if not nums:
+        return 0
+
+    i = 0 
+
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+
+    return i + 1  
+
+#Example
+nums = [1, 1, 2, 2, 3]
+k = removeDuplicates(nums)
+
+print(k)        
+print(nums[:k]) 
